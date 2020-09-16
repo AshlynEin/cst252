d9ddae518f387d25214e98f7062c777c7b6bea27
AshlynEin <70294913+AshlynEin@users.noreply.github.com>
2020-09-15 23:02:21 -0700

    lab 5
    damn daniel

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/* 
+ * Author: Ashlyn Einhell <aeinhell@csumb.edu>
+ *Created 15 September 2020
+ * License: Public Domain 
+ */
+
+ //Define Variables 
+ make="Vans";
+ model="Sk8-Hi";
+ color="Zeppelin";
+ year="2019";
+
+ //calculate 
+ age=2020-year;
+
+ //output 
+ document.writeln("Make:" + make + "<br>");
+ document.writeln( "Model:" + model + "<br>");
+ document.writeln( "Color:" + color + "<br>");
+ document.writeln( "Year:" + year + "<br>");
+ document.writeln( "Age:" + age + "years<br>");
+ 
