17712119202889a89fbbbc9a3df6682a20de37ae
AshlynEin <70294913+AshlynEin@users.noreply.github.com>
2020-09-21 00:27:39 -0700

    lab 6
    car memes baybee

---
+++ lab6/js/lab6.js
@@ -0,0 +1,25 @@
+/* 
+*Author: Ashlyn Einhell
+* Created: 20 September
+*License: Public Domain
+*/
+
+//Define Variables
+myTransport= ["feet", "mom car","bus"];
+
+myMainRide= {
+	make:'Vans',
+	model: 'Sk8-Hi',
+	color: 'Zeppelin',
+	year: '2019',
+	age: function() {
+		return 2020 - age;
+
+	}
+}
+
+//output
+document.writeln("Kinds of transportation I use: ",myTransport,"</br>");
+document.writeln("My Main Ride:<pre>",
+	JSON.stringify(myMainRide, null, '/t'), "</pre>");
+
