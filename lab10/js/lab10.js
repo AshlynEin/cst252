5e8ce601b6b25bcb878590d1fe7c4bedea7ffc83
AshlynEin <70294913+AshlynEin@users.noreply.github.com>
2020-10-05 00:36:33 -0700

    lab 10
    its 12:36AM and im listening to Katy Perry somebody help

---
+++ lab10/js/lab10.js
@@ -0,0 +1,30 @@
+// Ashlyn Einhell 
+//October 4th 2020
+// Credit to: Wes Modes and their lecture notes, Hannah and Eddie for help, and Kayla's friend Xue Ee for letting us use her code
+
+//create an event handler
+var button =  document.getElementById("my-button");
+console.log("Button baybee", button);
+
+button.addEventListener("click", function() {
+	//buttom is pressed and user is prompted to add their name to the function
+	var name = document.getElementById("user-name").value;
+	console.log("Name is", name);
+	//Changes from greeting to hello and user
+	var heading = document.getElementById("heading");
+	console.log("heading", heading);
+	heading.innerHTML = "Hello," + name;
+
+	//string to array
+	var nameArray = name.split('');
+	console.log("nameArray", nameArray);
+
+	//sorting
+	var nameArraySort = nameArray.sort();
+	console.log("nameArraySort=", nameArraySort);
+
+	//back together string
+	var nameSorted = nameArraysort.join('');
+	console.log("nameSorted=", nameSorted);
+	heading.innerHTML = "Hello, " + nameSorted;
+})
